c08be10dff2b350991a9c92ecc192b2ec7d9b22e
htmldiana <dianekhomenko@gmail.com>
2023-03-01 18:23:12 +0100

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body')
 
+const startTimer = event => {
+      timerColor = setInterval(() => {
+          console.log('event');
+          body.style.backgroundColor = getRandomHexColor();
+          document.querySelector('[data-start]').disabled = true;
+      }, 1000);
+};
+
+const stopTimer = event => {
+    clearInterval(timerColor);
+    document.querySelector('[data-start]').disabled = false;
+}
+
+startButton.addEventListener('click', startTimer)
+stopButton.addEventListener('click', stopTimer);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
